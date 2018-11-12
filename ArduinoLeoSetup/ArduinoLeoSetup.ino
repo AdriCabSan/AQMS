8b502344bad42473f48c81828a38c110a51cc1e7
adriancs <adriancabs98@gmail.com>
2018-11-12 02:17:21 -0600

    Feature:[charts]:Layout change to add more charts(Co2 was added)

--- ArduinoLeoSetup/ArduinoLeoSetup.ino
+++ ArduinoLeoSetup/ArduinoLeoSetup.ino
@@ -1,14 +1,16 @@
 int counter = 1;
-int counter2 = 100;
-int counter3= 200;
-int counter4 = 300;
+int counter2 = 0;
+int counter3= 0;
+int counter4 = 0;
 int incomingByte =0;
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  
+  if (counter2 > 40){
+    counter2=0;
+    }
   Serial.print(++counter,DEC);
   Serial.print(",");
   Serial.print(++counter2,DEC);
@@ -16,6 +18,6 @@ void loop() {
   Serial.print(++counter3,DEC);
   Serial.print(",");
   Serial.println(++counter4,DEC);
-  delay(2000);
+  delay(5000);
 
 }
--- Server/server.js
+++ Server/server.js
@@ -6,6 +6,7 @@ const app = express();
 const server =  http.createServer(app);
 const io = socketIO.listen(server);
 
+let temperature,tVOC,Co2,humidity;
 
 
 app.get('/',(req,res,next)=> {
@@ -19,28 +20,32 @@ const parser = new Readline();
 const mySerial = new SerialPort("COM4",{
     baudRate:9600
 });
-var temperature,tVOC,Co2,humidity;
-mySerial.on("open",() => console.log("Opened Serial Port"));
-io.on('connection',()=>{console.log('A new socket has connected');});
-mySerial.on("data",(data) => {
+
+
+server.listen(3000,()    => console.log('server on port: ',3000));
+io.on('connection',()    => console.log('A new socket has connected'));
+
+mySerial.on("err",err   => console.log(err.message));
+mySerial.on("open",()   => console.log("Opened Serial Port"));
+mySerial.on("data",data => {
+
     dataStream=data.toString();
     //console.log(dataStream);
-    var arr = dataStream.split(",").map((item) => {
-        return item.trim();
-      });
-      temperature=arr[0];
-      tVOC = arr[1];
-      Co2= arr[2];
-      humidity = arr[3];
-      console.log(`Temp:${temperature}, TVOC:${tVOC}, Co2:${Co2}, humidity:${humidity}`);
-      io.emit('arduino:temp',{
+    let arr = dataStream.split(",").map(item => item.trim());
+    temperature=arr[0];
+    tVOC = arr[1];
+    Co2= arr[2];
+    humidity = arr[3];
+    console.log(`Temp:${temperature}, TVOC:${tVOC}, Co2:${Co2}, humidity:${humidity}`);
+
+    io.emit('arduino:temp',{
         value:temperature
     });
     io.emit('arduino:tvoc',{
         value:tVOC
     });
     io.emit('arduino:co2',{
-         value:Co2
+        value:Co2
     });
     io.emit('arduino:hum',{
         value:humidity
@@ -48,5 +53,3 @@ mySerial.on("data",(data) => {
     
 });
 
-mySerial.on("err",(err) => {console.log(err.message);});
-server.listen(3000,() => {console.log('server on port: ',3000);});
