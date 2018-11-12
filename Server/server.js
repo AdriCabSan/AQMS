4a0169ddc48b4b73bd753bbf2ac83a3345449d95
adriancs <adriancabs98@gmail.com>
2018-11-11 23:02:42 -0600

    Feature[input]:Trim input string into variables

--- Server/server.js
+++ Server/server.js
@@ -6,7 +6,7 @@ const app = express();
 const server =  http.createServer(app);
 const io = socketIO.listen(server);
 
-io.on('connection',()=>{console.log('A new socket has connected');});
+
 
 app.get('/',(req,res,next)=> {
     res.sendFile(__dirname +'/index.html');
@@ -21,11 +21,31 @@ const mySerial = new SerialPort("COM4",{
 });
 var temperature,tVOC,Co2,humidity;
 mySerial.on("open",() => console.log("Opened Serial Port"));
+io.on('connection',()=>{console.log('A new socket has connected');});
 mySerial.on("data",(data) => {
-    console.log(data.toString());
-    io.emit('arduino:data',{
-        value:data.toString()
+    dataStream=data.toString();
+    //console.log(dataStream);
+    var arr = dataStream.split(",").map((item) => {
+        return item.trim();
+      });
+      temperature=arr[0];
+      tVOC = arr[1];
+      Co2= arr[2];
+      humidity = arr[3];
+      console.log(`Temp:${temperature}, TVOC:${tVOC}, Co2:${Co2}, humidity:${humidity}`);
+      io.emit('arduino:temp',{
+        value:temperature
     });
+    io.emit('arduino:tvoc',{
+        value:tVOC
+    });
+    io.emit('arduino:co2',{
+         value:Co2
+    });
+    io.emit('arduino:hum',{
+        value:humidity
+    });
+    
 });
 
 mySerial.on("err",(err) => {console.log(err.message);});
