be59a79bbe961eae45dbb9b1c8bfa3215e915551
adriancs <adriancabs98@gmail.com>
2019-04-04 10:14:00 -0600

    Feature[Proyect]:Reorganize project structure & add dates file

---
+++ public/js/dates.js
@@ -0,0 +1,18 @@
+var today = new Date();
+
+function formatDate(date) {
+    var monthNames = [
+      "Enero", "Febrero", "Marzo",
+      "Abril", "Mayo", "Junio", "Julio",
+      "Agosto", "Septiembre", "Octubre",
+      "Noviembre", "Diciembre"
+    ];
+  
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
+  
+    return day + ' ' + monthNames[monthIndex] + ' ' + year;
+  }
+let date=document.getElementById("currentDate");
+date.innerHTML=formatDate(today);
